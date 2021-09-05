12d1cef33470c0622e8666e604bce990fc33a4ff
Egidija <egidija@mail.com>
2021-09-05 22:48:24 +0300

    4 ND

---
+++ Page/W3MultiplePage.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+using autotests.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace PageObject.Page
+{
+    public class W3MultiplePage : BasePage
+    {
+        private const string AddressUrl = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+        private SelectElement carsDropdown => new SelectElement(Driver.FindElement(By.Id("cars")));
+        private IWebElement submitButton => Driver.FindElement(By.CssSelector("body > form > input[type=submit]"));
+        private IWebElement acceptCookiesButton => Driver.FindElement(By.Id("accept-choices"));
+        private IWebElement resultField => Driver.FindElement(By.CssSelector(".w3-container.w3-large.w3-border"));
+        private IWebElement runButton => Driver.FindElement(By.CssSelector(".w3-button.w3-bar-item.w3-hover-white.w3-hover-text-green"));
+        public W3MultiplePage(IWebDriver webdriver) : base(webdriver) { }
+        public void NavigateToPage()
+        {
+            if (Driver.Url != AddressUrl)
+            {
+                Driver.Url = AddressUrl;
+            }
+        }
+        public void ClickRunButton()
+        {
+            runButton.Click();
+        }
+        public void ClickSubmitButton()
+        {
+            submitButton.Click();
+        }
+        public void SelectFromDropDownByValue(List<string> cars)
+        {
+            Driver.SwitchTo().Frame("iframeResult");
+            carsDropdown.DeselectAll();
+            Actions action = new Actions(Driver);
+            action.KeyDown(Keys.Control);
+            foreach (IWebElement option in carsDropdown.Options)
+            {
+                if (cars.Contains(option.Text) && !option.Selected)
+                {
+                    option.Click();
+                }
+            }
+            action.KeyUp(Keys.Control);
+            action.Build().Perform();
+        }
+
+        public void VerifyResult(List<string> cars)
+        {
+            WaitForResult();
+            foreach (string car in cars)
+            {
+                Assert.IsTrue(resultField.Text.Contains(car.ToLower()),
+                    $"Result is wrong, was {resultField.Text}, but expected to contain {car}");
+            }
+        }
+
+        public void RerunPageScript()
+        {
+            Driver.SwitchTo().DefaultContent();
+            runButton.Click();
+        }
+       
+        public void AcceptCookies()
+        {
+            acceptCookiesButton.Click();
+        }
+
+        [System.Obsolete]
+        private void WaitForResult() => GetWait().Until(ExpectedConditions.ElementExists(By.CssSelector("body > p")));
+    }
+}
---
+++ Test/W3MultipleTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using PageObject.Page;
+
+namespace PageObject.Test
+{
+    public class W3MultipleTest 
+    {
+        private static W3MultiplePage page;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            page = new W3MultiplePage(chrome);
+            chrome.Url = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+            chrome.Manage().Window.Maximize();
+            WebDriverWait wait = new WebDriverWait(chrome, TimeSpan.FromSeconds(10));
+           
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            page.CloseBrowser();
+        }
+
+        [TestCase("Volvo", "Opel", TestName = "Select Volvo and Opel")]
+        [TestCase("Volvo", "Saab", "Opel", "Audi", TestName = "Select Volvo, Saab, Opel and Audi")]
+        [TestCase("Saab", "Audi", TestName = "Select Saab and Audi")]
+        public static void TestMultipleDropdown(params string[] cars)
+        {
+            page.NavigateToPage();
+            page.AcceptCookies();
+            page.SelectFromDropDownByValue(cars.ToList());
+            page.ClickSubmitButton();
+            page.VerifyResult(cars.ToList());
+            page.RerunPageScript();
+
+        }
+    }
+}
